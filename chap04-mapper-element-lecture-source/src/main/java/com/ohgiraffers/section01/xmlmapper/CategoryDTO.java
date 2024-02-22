de43e12bbbe629374a4c6c043e6c934e4d5c3b03
universe <hello.its.universe@gmail.com>
2024-02-22 10:37:25 +0900

    ch04 initial commit
    ch04 initial commit

---
+++ chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/CategoryDTO.java
@@ -0,0 +1,49 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+public class CategoryDTO {
+    private int categoryCode;
+    private String categoryName;
+    private Integer refCategoryCode;    // null 이 들어올 수 있는 필드는 wrapper 클래스로 작성해야 한다.
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(int categoryCode, String categoryName, Integer refCategoryCode) {
+        this.categoryCode = categoryCode;
+        this.categoryName = categoryName;
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getRefCategoryCode() {
+        return refCategoryCode;
+    }
+
+    public void setRefCategoryCode(Integer refCategoryCode) {
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryDTO{" +
+                "categoryCode=" + categoryCode +
+                ", categoryName='" + categoryName + '\'' +
+                ", refCategoryCode=" + refCategoryCode +
+                '}';
+    }
+}
